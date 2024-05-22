9c38f34a218326026eba112aa59335487da8060c
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-22 11:50:41 +0200

    Added find by id method and changed id type from Long to String.

--- src/main/java/pl/pja/s28201/tpo_10/repository/UrlObjectRepository.java
+++ src/main/java/pl/pja/s28201/tpo_10/repository/UrlObjectRepository.java
@@ -5,9 +5,12 @@ import org.springframework.lang.NonNull;
 import pl.pja.s28201.tpo_10.model.UrlObject;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface UrlObjectRepository extends CrudRepository<UrlObject, Long> {
+public interface UrlObjectRepository extends CrudRepository<UrlObject, String> {
 
     @NonNull
     List<UrlObject> findAll();
+    @NonNull
+    Optional<UrlObject> findById(@NonNull String id);
 }
