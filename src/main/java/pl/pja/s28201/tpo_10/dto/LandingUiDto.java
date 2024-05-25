e72d83daa9bed7f1ea095b4fa35dc3cfd43993a1
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-25 19:43:24 +0200

    Added ui dto to handle page translation.

---
+++ src/main/java/pl/pja/s28201/tpo_10/dto/LandingUiDto.java
@@ -0,0 +1,97 @@
+package pl.pja.s28201.tpo_10.dto;
+
+
+public class LandingUiDto {
+
+    private String chooseLanguageHeader;
+    private String englishButton;
+    private String polishButton;
+    private String germanButton;
+    private String submitFormHeader;
+
+    private String nameLabel;
+    private String passwordLabel;
+    private String targetUrlLabel;
+    private String buttonSubmit;
+    private String buttonRedirect;
+
+    public String getTargetUrlLabel() {
+        return targetUrlLabel;
+    }
+
+    public void setTargetUrlLabel(String targetUrlLabel) {
+        this.targetUrlLabel = targetUrlLabel;
+    }
+
+    public String getChooseLanguageHeader() {
+        return chooseLanguageHeader;
+    }
+
+    public void setChooseLanguageHeader(String chooseLanguageHeader) {
+        this.chooseLanguageHeader = chooseLanguageHeader;
+    }
+
+    public String getEnglishButton() {
+        return englishButton;
+    }
+
+    public void setEnglishButton(String englishButton) {
+        this.englishButton = englishButton;
+    }
+
+    public String getPolishButton() {
+        return polishButton;
+    }
+
+    public void setPolishButton(String polishButton) {
+        this.polishButton = polishButton;
+    }
+
+    public String getGermanButton() {
+        return germanButton;
+    }
+
+    public void setGermanButton(String germanButton) {
+        this.germanButton = germanButton;
+    }
+
+    public String getSubmitFormHeader() {
+        return submitFormHeader;
+    }
+
+    public void setSubmitFormHeader(String submitFormHeader) {
+        this.submitFormHeader = submitFormHeader;
+    }
+
+    public String getNameLabel() {
+        return nameLabel;
+    }
+
+    public void setNameLabel(String nameLabel) {
+        this.nameLabel = nameLabel;
+    }
+
+    public String getPasswordLabel() {
+        return passwordLabel;
+    }
+
+    public void setPasswordLabel(String passwordLabel) {
+        this.passwordLabel = passwordLabel;
+    }
+
+    public String getButtonSubmit() {
+        return buttonSubmit;
+    }
+
+    public void setButtonSubmit(String buttonSubmit) {
+        this.buttonSubmit = buttonSubmit;
+    }
+
+    public String getButtonRedirect() {
+        return buttonRedirect;
+    }
+
+    public void setButtonRedirect(String buttonRedirect) {
+        this.buttonRedirect = buttonRedirect;
+    }
+}
