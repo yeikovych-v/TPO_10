1264538a15d11a11d0436dedfcaf4bc3ac5668c4
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-25 19:44:27 +0200

    Added validation to url dto object.

--- src/main/java/pl/pja/s28201/tpo_10/dto/UrlObjectDto.java
+++ src/main/java/pl/pja/s28201/tpo_10/dto/UrlObjectDto.java
@@ -1,8 +1,20 @@
 package pl.pja.s28201.tpo_10.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+import pl.pja.s28201.tpo_10.validation.password.StrongPassword;
+import pl.pja.s28201.tpo_10.validation.url.HttpsUrl;
+
 public class UrlObjectDto {
+    @Length(min = 5, max = 20)
     private String name;
+
+    @StrongPassword(lowerMin = 1, upperMin = 2, numbersMin = 3, specialMin = 4)
     private String password;
+    @URL
+    @HttpsUrl
+    @NotEmpty
     private String targetUrl;
 
     public String getPassword() {
@@ -27,5 +39,14 @@ public class UrlObjectDto {
 
     public void setTargetUrl(String targetUrl) {
         this.targetUrl = targetUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "UrlObjectDto{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", targetUrl='" + targetUrl + '\'' +
+                '}';
     }
 }
