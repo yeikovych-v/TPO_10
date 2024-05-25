72974bcff5b02023bf5775caeaf6f23e70d8e85d
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-25 19:41:21 +0200

    Added rest config.

---
+++ src/main/java/pl/pja/s28201/tpo_10/config/RestConfig.java
@@ -0,0 +1,14 @@
+package pl.pja.s28201.tpo_10.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
