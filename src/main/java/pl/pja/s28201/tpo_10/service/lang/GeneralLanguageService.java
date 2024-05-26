940d04a02a72e06da514542a54a99176fda3e508
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-26 14:27:53 +0200

    Implemented methods to handle translation.

--- src/main/java/pl/pja/s28201/tpo_10/service/lang/GeneralLanguageService.java
+++ src/main/java/pl/pja/s28201/tpo_10/service/lang/GeneralLanguageService.java
@@ -1,14 +1,17 @@
 package pl.pja.s28201.tpo_10.service.lang;
 
 import org.springframework.stereotype.Service;
-import pl.pja.s28201.tpo_10.dto.LandingUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.AuthUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.LandingUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.SettingsUiDto;
 import pl.pja.s28201.tpo_10.model.Language;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
-public class GeneralLanguageService implements ILangService{
+public class GeneralLanguageService implements ILangService {
 
     private Language currentAppLanguage = Language.EN;
     private static final Map<Language, ILangService> SERVICE_MAP = new HashMap<>();
@@ -21,6 +24,7 @@ public class GeneralLanguageService implements ILangService{
 
     public void changeAppLanguage(Language to) {
         currentAppLanguage = to;
+        Locale.setDefault(Locale.forLanguageTag(to.name()));
     }
 
     @Override
@@ -31,5 +35,40 @@ public class GeneralLanguageService implements ILangService{
     @Override
     public LandingUiDto getLandingDto() {
         return SERVICE_MAP.get(currentAppLanguage).getLandingDto();
+    }
+
+    @Override
+    public AuthUiDto getAuthDto() {
+        return SERVICE_MAP.get(currentAppLanguage).getAuthDto();
+    }
+
+    @Override
+    public SettingsUiDto getSettingsDto() {
+        return SERVICE_MAP.get(currentAppLanguage).getSettingsDto();
+    }
+
+    @Override
+    public String getInvalidPasswordErrorMessage() {
+        return SERVICE_MAP.get(currentAppLanguage).getInvalidPasswordErrorMessage();
+    }
+
+    @Override
+    public String getLinkCreatedMessage() {
+        return SERVICE_MAP.get(currentAppLanguage).getLinkCreatedMessage();
+    }
+
+    @Override
+    public String getNotFoundErrorMessage() {
+        return SERVICE_MAP.get(currentAppLanguage).getNotFoundErrorMessage();
+    }
+
+    @Override
+    public String getUrlAlreadyExistsErrorMessage() {
+        return SERVICE_MAP.get(currentAppLanguage).getUrlAlreadyExistsErrorMessage();
+    }
+
+    @Override
+    public String getLinkDeletedMessage() {
+        return SERVICE_MAP.get(currentAppLanguage).getLinkDeletedMessage();
     }
 }
