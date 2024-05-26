50d5dc766c73b869c23438458a63d8536f2178ee
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-26 14:27:33 +0200

    Added methods to handle translation.

--- src/main/java/pl/pja/s28201/tpo_10/service/lang/ILangService.java
+++ src/main/java/pl/pja/s28201/tpo_10/service/lang/ILangService.java
@@ -1,10 +1,18 @@
 package pl.pja.s28201.tpo_10.service.lang;
 
-import pl.pja.s28201.tpo_10.dto.LandingUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.AuthUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.LandingUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.SettingsUiDto;
 
 public interface ILangService {
 
     String getLanguageCode();
-
     LandingUiDto getLandingDto();
+    AuthUiDto getAuthDto();
+    SettingsUiDto getSettingsDto();
+    String getInvalidPasswordErrorMessage();
+    String getLinkCreatedMessage();
+    String getNotFoundErrorMessage();
+    String getUrlAlreadyExistsErrorMessage();
+    String getLinkDeletedMessage();
 }
