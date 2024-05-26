e894e8f33e0be34b3bf555d4213cc6062d47f7ec
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-26 14:24:32 +0200

    Added ui settings dto to handle settings ui change on locale change.

---
+++ src/main/java/pl/pja/s28201/tpo_10/dto/ui/SettingsUiDto.java
@@ -0,0 +1,77 @@
+package pl.pja.s28201.tpo_10.dto.ui;
+
+public class SettingsUiDto {
+
+    private String settingsHeader;
+    private String nameLabel;
+    private String targetUrlLabel;
+    private String redirectUrlLabel;
+    private String visitsLabel;
+    private String buttonSubmit;
+    private String buttonCancel;
+    private String buttonDelete;
+
+    public String getSettingsHeader() {
+        return settingsHeader;
+    }
+
+    public void setSettingsHeader(String settingsHeader) {
+        this.settingsHeader = settingsHeader;
+    }
+
+    public String getNameLabel() {
+        return nameLabel;
+    }
+
+    public void setNameLabel(String nameLabel) {
+        this.nameLabel = nameLabel;
+    }
+
+    public String getTargetUrlLabel() {
+        return targetUrlLabel;
+    }
+
+    public void setTargetUrlLabel(String targetUrlLabel) {
+        this.targetUrlLabel = targetUrlLabel;
+    }
+
+    public String getRedirectUrlLabel() {
+        return redirectUrlLabel;
+    }
+
+    public void setRedirectUrlLabel(String redirectUrlLabel) {
+        this.redirectUrlLabel = redirectUrlLabel;
+    }
+
+    public String getVisitsLabel() {
+        return visitsLabel;
+    }
+
+    public void setVisitsLabel(String visitsLabel) {
+        this.visitsLabel = visitsLabel;
+    }
+
+    public String getButtonSubmit() {
+        return buttonSubmit;
+    }
+
+    public void setButtonSubmit(String buttonSubmit) {
+        this.buttonSubmit = buttonSubmit;
+    }
+
+    public String getButtonCancel() {
+        return buttonCancel;
+    }
+
+    public void setButtonCancel(String buttonCancel) {
+        this.buttonCancel = buttonCancel;
+    }
+
+    public String getButtonDelete() {
+        return buttonDelete;
+    }
+
+    public void setButtonDelete(String buttonDelete) {
+        this.buttonDelete = buttonDelete;
+    }
+}
