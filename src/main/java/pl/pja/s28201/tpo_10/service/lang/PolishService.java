78a12544a1135b525f02bcd5121a462f4bdb42f6
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-05-26 14:26:12 +0200

    Added more error codes and 2 methods to support polish ui.

--- src/main/java/pl/pja/s28201/tpo_10/service/lang/PolishService.java
+++ src/main/java/pl/pja/s28201/tpo_10/service/lang/PolishService.java
@@ -1,7 +1,9 @@
 package pl.pja.s28201.tpo_10.service.lang;
 
 import org.springframework.stereotype.Service;
-import pl.pja.s28201.tpo_10.dto.LandingUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.AuthUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.LandingUiDto;
+import pl.pja.s28201.tpo_10.dto.ui.SettingsUiDto;
 
 @Service
 public class PolishService implements ILangService {
@@ -27,5 +29,61 @@ public class PolishService implements ILangService {
         landingDto.setButtonRedirect("Edytuj URL-e");
 
         return landingDto;
+    }
+
+    @Override
+    public AuthUiDto getAuthDto() {
+        var authDto = new AuthUiDto();
+
+        authDto.setAuthHeader("Autoryzacja");
+        authDto.setUrlLabel("Podaj nazwę URL:");
+        authDto.setUrlPlaceHolder("Wprowadź URL");
+        authDto.setPasswordLabel("Hasło:");
+        authDto.setPasswordPlaceHolder("Wprowadź hasło");
+
+        authDto.setButtonSubmit("Zatwierdź");
+        authDto.setButtonCancel("Anuluj");
+        return authDto;
+    }
+
+    @Override
+    public SettingsUiDto getSettingsDto() {
+        var settingsDto = new SettingsUiDto();
+
+        settingsDto.setSettingsHeader("Autoryzacja");
+        settingsDto.setNameLabel("Nazwa URL:");
+        settingsDto.setTargetUrlLabel("Docelowy URL:");
+        settingsDto.setRedirectUrlLabel("URL przekierowania:");
+        settingsDto.setVisitsLabel("Liczba wizyt:");
+
+        settingsDto.setButtonSubmit("Zapisz zmiany");
+        settingsDto.setButtonCancel("Wróć");
+        settingsDto.setButtonDelete("Usuń");
+        return settingsDto;
+    }
+
+    @Override
+    public String getInvalidPasswordErrorMessage() {
+        return "Nieprawidłowe hasło lub nazwa użytkownika.";
+    }
+
+    @Override
+    public String getLinkCreatedMessage() {
+        return "Link został pomyślnie utworzony.";
+    }
+
+    @Override
+    public String getNotFoundErrorMessage() {
+        return "404 Nie znaleziono";
+    }
+
+    @Override
+    public String getUrlAlreadyExistsErrorMessage() {
+        return "Podany URL już istnieje w systemie.";
+    }
+
+    @Override
+    public String getLinkDeletedMessage() {
+        return "Link został pomyślnie usunięty.";
     }
 }
